7db4d466f3d2bc6752aba21ce5d0b4c945a603f0
fogoplayer <fogoplayer@gmail.com>
2017-09-04 10:31:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,26 @@ var storage = localStorage;
 
 var save = function(){
   var text = document.getElementsByClassName("textArea");
-  for (i=0;i<text.length();i++){
-    storage.setItem(text[i].id) = text[i].innerText;
-    console.log(text[i].innerText);
+  for (i=0;i<text.length;i++){
+    storage.setItem(text[i].id, text[i].innerText);
   }
-}
+  console.log("saving");
+};
 
 var load = function (){
   var text = document.getElementsByClassName("textArea");
-  for (i=0;i<text.length();i++){
-    text[i].innerText = storage.getItem(text[i].id)
-    console.log(text[i].innerText);
+  for (i=0;i<text.length;i++){
+    if (storage.getItem(text[i].id) !== undefined){
+      text[i].innerText = storage.getItem(text[i].id);
+    }else{
+      text[i].innerText = "";
+    }
+    
+    console.log(text[i]);
+    text[i].addEventListener("keyup", save);
   }
-}
+};
+
+document.addEventListener("DOMContentLoaded", function(){
+  load();
+});
