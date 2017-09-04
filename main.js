c8ed729afb9fcfaa9417dec1a447992338165e69
fogoplayer <fogoplayer@gmail.com>
2017-09-04 07:53:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+var storage = localStorage;
 
+var save = function(){
+  var text = document.getElementsByClassName("textArea");
+  for (i=0;i<text.length();i++){
+    storage.setItem(text[i].id) = text[i].innerText;
+    console.log(text[i].innerText);
+  }
+}
+
+var load = function (){
+  var text = document.getElementsByClassName("textArea");
+  for (i=0;i<text.length();i++){
+    text[i].innerText = storage.getItem(text[i].id)
+    console.log(text[i].innerText);
+  }
+}
