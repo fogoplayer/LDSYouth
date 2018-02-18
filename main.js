0446e74359c64ee7c20180f788cb451636d3e767
Zarin Loosli <fogoplayer@gmail.com>
2018-02-18 12:11:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var storage = localStorage;
 
-//Take the innerHTML of all textArea divs and saves it to local storage
+/*
+Saves contents of divs on the page to local storage
+*/
 function save() {
     var text = document.getElementsByClassName("textArea");
     for (i = 0; i < text.length; i++) {
@@ -8,24 +10,24 @@ function save() {
     }
 }
 
-//See if textArea divs have local storage data, if so, populate them
+/*
+See if textArea divs have local storage data, if so, populate them
+*/
 function load() {
     var text = document.getElementsByClassName("textArea");
     for (i = 0; i < text.length; i++) {
         if (storage.getItem(text[i].id) !== (undefined || null)) {
             text[i].innerHTML = storage.getItem(text[i].id);
         }
-
         text[i].addEventListener("keyup", save);
     }
 }
 
+/*
+Load data into progress tracker
+//TODO: Rewrite to be more modular with goal of eventually adding DTG tracker
+*/
 function loadProgress() {
-    var text = document.getElementsByClassName("progressCell");
-    for (i = 0; i < text.length; i++) {
-
-    }
-    //Faith
     scanStorage("faith");
     scanStorage("divineNature");
     scanStorage("individualWorth");
@@ -35,18 +37,18 @@ function loadProgress() {
     scanStorage("integrity");
     scanStorage("virtue");
 }
-
+/*
+Sub-function of loadProgress for each YW Value
+@param val - The value being scanned for
+*/
 function scanStorage(val) {
     var value;
     var element;
-
     //Loop through storage and find all of the signed off numbers
     for (i = 1; i < 10; i++) {
         value = storage[val + "Signoff" + i.toString()];
-
         //If the storage slot has something in it, then...
         if (value !== undefined && value !== "") {
-
             //...loop through the record sheet, find the next empty box, and put the value in it
             for (j = 1; j < 6; j++) {
                 element = document.getElementById(val + j.toString());
@@ -57,7 +59,6 @@ function scanStorage(val) {
             }
         }
     }
-
     //Access storage slot for the value project
     value = storage[val + "SignoffVP"];
     //If the storage slot has something in it, then...
@@ -68,20 +69,4 @@ function scanStorage(val) {
             j = 9999;
         }
     }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    if (document.getElementsByClassName("textArea")) {
-        load();
-    }
-
-    if (document.getElementsByClassName("progressCell")) {
-        loadProgress();
-    }
-});
-
-//Register Service Worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('./service-worker.js').then(function() { console.log('Service Worker registered'); });
 }
