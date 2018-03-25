bd4852a0a0e2f408946ada0e245f4899f5557c70
EC2 Default User <ec2-user@ip-172-31-44-19.us-east-2.compute.internal>
2018-03-25 16:09:51 +0000

    trying to fix CSS reference issues

---
+++ main.js
@@ -0,0 +1,82 @@
+var storage = localStorage;
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (document.getElementsByClassName("textArea")){
+        load();
+    }
+    
+    if (document.getElementsByName("progressCell")){
+        loadProgress();
+    }
+});
+
+/*
+Saves contents of divs on the page to local storage
+*/
+function save() {
+    var text = document.getElementsByClassName("textArea");
+    for (i = 0; i < text.length; i++) {
+        storage.setItem(text[i].id, text[i].innerHTML);
+    }
+}
+
+/*
+See if textArea divs have local storage data, if so, populate them
+*/
+function load() {
+    var text = document.getElementsByClassName("textArea");
+    for (i = 0; i < text.length; i++) {
+        if (storage.getItem(text[i].id) !== (undefined || null)) {
+            text[i].innerHTML = storage.getItem(text[i].id);
+        }
+        text[i].addEventListener("keyup", save);
+    }
+}
+
+/*
+Load data into progress tracker
+//TODO: Rewrite to be more modular with goal of eventually adding DTG tracker
+*/
+function loadProgress() {
+    scanStorage("faith");
+    scanStorage("divineNature");
+    scanStorage("individualWorth");
+    scanStorage("knowledge");
+    scanStorage("choiceAndAccountability");
+    scanStorage("goodWorks");
+    scanStorage("integrity");
+    scanStorage("virtue");
+}
+/*
+Sub-function of loadProgress for each YW Value
+@param val - The value being scanned for
+*/
+function scanStorage(val) {
+    var value;
+    var element;
+    //Loop through storage and find all of the signed off numbers
+    for (i = 1; i < 10; i++) {
+        value = storage[val + "Signoff" + i.toString()];
+        //If the storage slot has something in it, then...
+        if (value !== undefined && value !== "") {
+            //...loop through the record sheet, find the next empty box, and put the value in it
+            for (j = 1; j < 6; j++) {
+                element = document.getElementById(val + j.toString());
+                if (element.innerText === "") {
+                    element.innerText = value;
+                    j = 9999;
+                }
+            }
+        }
+    }
+    //Access storage slot for the value project
+    value = storage[val + "SignoffVP"];
+    //If the storage slot has something in it, then...
+    if (value !== undefined && value !== "") {
+        element = document.getElementById(val + "VP");
+        if (element.innerText === "") {
+            element.innerText = value;
+            j = 9999;
+        }
+    }
+}
---
+++ service-worker.js
@@ -0,0 +1,47 @@
+var cacheName = 'LDSYouth';
+var dataCacheName = "LDSYouth-v5";
+var filesToCache = [
+    '/',
+    '/LDSYouth/',
+    'index.html',
+    'main.js',
+    'styles.css',
+    'manifest.json',
+    'materialize.min.css',
+    'materialize.min.js',
+    'images/dtg.jpg',
+    'images/pp.jpg'
+];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
